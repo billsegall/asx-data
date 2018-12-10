b71f7ad7a608e08657e6aa193c8325a1b8da68ad
billsegall <bill@segall.net>
2018-12-10 07:43:08 +0000

    Strip the days ASIC deems as bad.

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # asx-shorts
 ASX short lists processing
 
-The **data/0.raw** data files were obtained from [ASIC](https://asic.gov.au/regulatory-resources/markets/short-selling/short-selling-reports-notice/), and contain some [inaccuracies](https://asic.gov.au/regulatory-resources/markets/short-selling/short-selling-reports-notice/).
+The **data/0.raw** data files were obtained from
+[ASIC](https://asic.gov.au/regulatory-resources/markets/short-selling/short-selling-reports-notice/), and
+contains some [inaccuracies](https://asic.gov.au/regulatory-resources/markets/short-selling/short-selling-reports-notice/).
 
 These were processed modified for consistency to **data/1.ascii** using LibreOffice.
 
-Then *cvs2jon.py* was used to load that and convert it to useful json on a per year basis. See the *Makefile*
-for how that's done and the differences per year.
+Then, *cvs2json.py* was used to load that and convert it to json on a per year basis in **data/2.json**. See
+the *Makefile* for how that's done and the differences per year. It also strips out the days which ASIC
+reported as having bad data.
--- csv2json.py
+++ csv2json.py
@@ -7,7 +7,7 @@ import argparse, csv, json, time, sys
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Filter the ASX short positions lists')
     parser.add_argument('--infile', type=argparse.FileType('r', encoding='ascii'), default=sys.stdin, help='input CSV')
-    parser.add_argument('--outfile', type=argparse.FileType('w'), default=sys.stdout, help='output CSV')
+    parser.add_argument('--outfile', type=argparse.FileType('w'), default=sys.stdout, help='output JSON')
     parser.add_argument('--dateformat', default='%d/%m/%Y', help='Format of the data (strptime)')
     args = parser.parse_args()
     # print(args)
@@ -21,8 +21,20 @@ if __name__ == "__main__":
     #
     # We end up with dict[code] -> [date, %]
 
-    reader = csv.reader(args.infile)
+    # Days with bad data
+    # https://asic.gov.au/regulatory-resources/markets/short-selling/short-selling-reports-notice/
+    bad = [ "19 June 2017", "16 June 2017", "15 June 2017", "1 November 2016", "3 October 2016",
+    "6 October 2014", "2 September 2014", "1 September 2014", "29 August 2014", "15 November 2013",
+    "7 October 2013", "28 June 2013", "27 June 2013", "26 June 2013", "25 June 2013", "24 June 2013",
+    "21 June 2013", "20 June 2013", "19 June 2013", "18 June 2013", "17 June 2013", "14 June 2013",
+    "13 June 2013", "7 June 2013", "22 October 2012", "19 October 2012", "18 October 2012", "17 October 2012",
+    "16 October 2012", "15 October 2012", "12 October 2012", "11 October 2012", "12 March 2012"]
+    baddates = []
+    for date in bad:
+        baddates.append(time.mktime(time.strptime(date, "%d %B %Y")))
+
     dates = []
+    reader = csv.reader(args.infile)
     d_shorts = {}
     for row in reader:
 
@@ -41,7 +53,10 @@ if __name__ == "__main__":
         elif reader.line_num == 2:
             for date in row[2::2]: # Every second
                 dt = time.mktime(time.strptime(date, args.dateformat))
-                dates.append(dt)
+                if not dt in baddates:
+                    dates.append(dt)
+                else:
+                    dates.append(0)
 
         # Header 3, Another descriptor but check it
         elif reader.line_num == 3:
@@ -57,11 +72,14 @@ if __name__ == "__main__":
 
         # short data to add to our dictionary
         else:
-            d_shorts[row[1]] = []
+            ticker = row[1].strip()
+            d_shorts[ticker] = []
             date_index = 0
             for percent in row[3::2]: # Every second
                 if percent != '': # Lots of empty days
-                    d_shorts[row[1]].append((dates[date_index], percent))
+                    if dates[date_index] != 0: # Don't add days ASIC said had bad data
+                        d_shorts[ticker].append((dates[date_index], percent))
+                        
                 date_index += 1
 
     json.dump(d_shorts, args.outfile)
