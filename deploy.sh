3874d21c3b246d4fddd99e49690d1b06d88cca38
billsegall <bill@segall.net>
2026-03-03 08:42:28 +1000

    Add deploy.sh for auto-pull and restart of asx-backend

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+cd /home/bill/code/asx-data
+git fetch origin
+LOCAL=$(git rev-parse HEAD)
+REMOTE=$(git rev-parse origin/master)
+if [ "$LOCAL" != "$REMOTE" ]; then
+    git pull --ff-only origin master
+    sudo systemctl restart asx-backend
+    echo "$(date): deployed $REMOTE"
+fi
